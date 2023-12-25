9e57e7f6b694439e7b9b7f2b72d2965a3062bb55
Astesana <jm-dev@astesana.net>
2023-12-25 10:33:53 +0100

    Changes DefaultMoveComparator to DefaultMoveEvaluator

--- src/main/java/com/fathzer/chess/utils/DefaultMoveEvaluator.java
+++ src/main/java/com/fathzer/chess/utils/DefaultMoveEvaluator.java
@@ -1,16 +1,16 @@
 package com.fathzer.chess.utils;
 
-import java.util.Comparator;
+import java.util.function.ToIntFunction;
 
 import com.fathzer.chess.utils.adapters.MoveAdapter;
 import com.fathzer.chess.utils.adapters.PieceEvaluator;
 
-/** A move comparator that considers a catch is better than other moves and taking a high value piece with a small value piece is better than the opposite.
+/** A move evaluator that considers a catch is better than other moves and taking a high value piece with a small value piece is better than the opposite.
  * @param <M> The type of moves
  * @param <B> The type of chess board
  * @param <P> The class of a Piece
  */
-public abstract class DefaultMoveComparator<M, B, P> implements Comparator<M>, PieceEvaluator<P>, MoveAdapter<M, B, P> {
+public abstract class DefaultMoveEvaluator<M, B, P> implements ToIntFunction<M>, PieceEvaluator<P>, MoveAdapter<M, B, P> {
 	/** The chess board on which the comparison are made.
 	 */
 	protected final B board;
@@ -18,14 +18,8 @@ public abstract class DefaultMoveComparator<M, B, P> implements Comparator<M>, P
 	/** Constructor.
 	 * @param board The chess board on which the comparison is made.
 	 */
-	protected DefaultMoveComparator(B board) {
+	protected DefaultMoveEvaluator(B board) {
 		this.board = board;
-	}
-
-	@Override
-	public int compare(M m1, M m2) {
-		// Important sort from higher to lower scores
-		return getMoveValue(m2) - getMoveValue(m1);
 	}
 
 	/** Gets the value of a move.
@@ -33,12 +27,13 @@ public abstract class DefaultMoveComparator<M, B, P> implements Comparator<M>, P
 	 * @return a value for a move.
 	 * <br>The default implementation guarantees that the returned value is always &gt;=0, and &gt;0 for promotions and captures.
 	 */
-	public int getMoveValue(M move) {
+	@Override
+	public int applyAsInt(M move) {
 		final P promotion = getPromotion(board, move);
 		int value = isNone(promotion) ? 0 : (getValue(promotion)-1)*16;
 		final P captured = getCaptured(board, move);
 		if (isNone(captured)) {
-			return value;
+			return value==0 ? Integer.MIN_VALUE : value;
 		} else {
 			value += getValue(captured)*16;
 			final P catching = getMoving(board, move);
